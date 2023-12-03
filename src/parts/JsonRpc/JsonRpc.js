4aafea3903fa9c2181e5e799fb17b470afdd2f6f
Le Vivilet <levivlet@pm.me>
2023-12-03 19:43:24 +0100

    feature: export getErrorResponse function

--- src/parts/JsonRpc/JsonRpc.js
+++ src/parts/JsonRpc/JsonRpc.js
@@ -25,3 +25,4 @@ export const invokeAndTransfer = async (ipc, handle, method, ...params) => {
 
 export { resolve } from '../Callback/Callback.js'
 export { handleJsonRpcMessage } from '../HandleJsonRpcMessage/HandleJsonRpcMessage.js'
+export { getErrorResponse } from '../GetErrorResponse/GetErrorResponse.js'
