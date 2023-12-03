a5ac9983ba83174da43d0ee3ed0d7abde58d359e
Le Vivilet <levivlet@pm.me>
2023-12-03 15:51:25 +0100

    fix: export resolve method

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,16 @@
-export const invoke: (method: string, ...params: any[]) => Promise<any>
+export const send: (ipc: any, method: string, ...params: any[]) => void
+
+export const invoke: (
+  ipc: any,
+  method: string,
+  ...params: any[]
+) => Promise<any>
+
+export const invokeAndTransfer: (
+  ipc: any,
+  transfer: any,
+  method: string,
+  ...params: any[]
+) => Promise<any>
 
 export const resolve: (id: number, message: any) => void
