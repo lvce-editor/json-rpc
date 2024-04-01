fa37077b337bdbc763ce0709ce5127a810cde4d6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-01 13:15:42 +0200

    feature: move callback state into separate file (#14)

--- src/parts/Callback/Callback.js
+++ src/parts/Callback/Callback.js
@@ -1,26 +1,23 @@
 import * as Assert from '../Assert/Assert.js'
+import * as CallbackState from '../CallbackState/CallbackState.js'
 import * as Id from '../Id/Id.js'
 import * as Logger from '../Logger/Logger.js'
 import * as Promises from '../Promises/Promises.js'
 
-export const state = {
-  callbacks: Object.create(null),
-}
-
 export const registerPromise = () => {
   const id = Id.create()
   const { resolve, promise } = Promises.withResolvers()
-  state.callbacks[id] = resolve
+  CallbackState.state.callbacks[id] = resolve
   return { id, promise }
 }
 
 export const resolve = (id, args) => {
   Assert.number(id)
-  if (!(id in state.callbacks)) {
+  if (!(id in CallbackState.state.callbacks)) {
     console.log(args)
     Logger.warn(`callback ${id} may already be disposed`)
     return
   }
-  state.callbacks[id](args)
-  delete state.callbacks[id]
+  CallbackState.state.callbacks[id](args)
+  delete CallbackState.state.callbacks[id]
 }
