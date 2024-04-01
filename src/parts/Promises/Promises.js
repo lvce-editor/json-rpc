b0e354a2a461d8909b59ff84e4505d9cb0ae4db5
Le Vivilet <levivlet@pm.me>
2024-04-01 13:17:20 +0200

    feature: remove unused promise reject code

--- src/parts/Promises/Promises.js
+++ src/parts/Promises/Promises.js
@@ -3,17 +3,11 @@ export const withResolvers = () => {
    * @type {any}
    */
   let _resolve
-  /**
-   * @type {any}
-   */
-  let _reject
-  const promise = new Promise((resolve, reject) => {
+  const promise = new Promise((resolve) => {
     _resolve = resolve
-    _reject = reject
   })
   return {
     resolve: _resolve,
-    reject: _reject,
     promise,
   }
 }
