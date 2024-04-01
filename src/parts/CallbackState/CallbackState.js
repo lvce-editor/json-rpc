14ef9e747631f3141ef91b994066193207e932cd
Le Vivilet <levivlet@pm.me>
2024-04-01 13:14:47 +0200

    feature: move callback state into separate file

---
+++ src/parts/CallbackState/CallbackState.js
@@ -0,0 +1,8 @@
+import * as Assert from '../Assert/Assert.js'
+import * as Id from '../Id/Id.js'
+import * as Logger from '../Logger/Logger.js'
+import * as Promises from '../Promises/Promises.js'
+
+export const state = {
+  callbacks: Object.create(null),
+}
