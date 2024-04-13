4d1b752986a1c5fdb249d726698b83b0bf16b014
Le Vivilet <levivlet@pm.me>
2024-04-13 19:27:53 +0200

    fix import

--- src/parts/Callback/Callback.ts
+++ src/parts/Callback/Callback.ts
@@ -2,7 +2,6 @@ import * as Assert from '../Assert/Assert.ts'
 import * as CallbackState from '../CallbackState/CallbackState.ts'
 import * as Id from '../Id/Id.ts'
 import * as Logger from '../Logger/Logger.ts'
-import * as Promises from '../Promises/Promises.ts'
 
 export const registerPromise = () => {
   const id = Id.create()
