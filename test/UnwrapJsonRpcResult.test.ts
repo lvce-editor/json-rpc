7b07c6013235424d39f31765eb09f0ee4513b443
Le Vivilet <levivlet@pm.me>
2024-07-15 22:52:42 +0200

    feature: add unwrapJsonRpcResult test

---
+++ test/UnwrapJsonRpcResult.test.ts
@@ -0,0 +1,28 @@
+import { expect, test, beforeEach, jest } from '@jest/globals'
+import * as Callback from '../src/parts/Callback/Callback.js'
+import * as UnwrapJsonRpcResult from '../src/parts/UnwrapJsonRpcResult/UnwrapJsonRpcResult.js'
+
+test('unwrapJsonRpcResult - error', () => {
+  const response = {
+    error: {
+      message: 'x is not a function',
+    },
+  }
+  expect(() => UnwrapJsonRpcResult.unwrapJsonRpcResult(response)).toThrow(
+    new Error('x is not a function'),
+  )
+})
+
+test('unwrapJsonRpcResult - result', () => {
+  const response = {
+    result: 12,
+  }
+  expect(UnwrapJsonRpcResult.unwrapJsonRpcResult(response)).toBe(12)
+})
+
+test('unwrapJsonRpcResult - unexpected response', () => {
+  const response = {}
+  expect(() => UnwrapJsonRpcResult.unwrapJsonRpcResult(response)).toThrow(
+    new Error('unexpected response message'),
+  )
+})
