2d3fd0b86305c19bde4126866d78366a0ca40a78
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-27 00:00:47 +0200

    feature: allow passing handleJsonRpcMessage parameters as object to m… (#40)

--- src/parts/HandleJsonRpcMessage/HandleJsonRpcMessage.ts
+++ src/parts/HandleJsonRpcMessage/HandleJsonRpcMessage.ts
@@ -1,16 +1,48 @@
+import * as Callback from '../Callback/Callback.ts'
 import * as GetErrorResponse from '../GetErrorResponse/GetErrorResponse.ts'
 import * as GetResponse from '../GetResponse/GetResponse.ts'
 import { JsonRpcError } from '../JsonRpcError/JsonRpcError.ts'
 
-export const handleJsonRpcMessage = async (
-  ipc: any,
-  message: any,
-  execute: any,
-  resolve: any,
-  preparePrettyError: any,
-  logError: any,
-  requiresSocket: any,
-) => {
+const defaultPreparePrettyError = (error: any) => {
+  return error
+}
+
+const defaultLogError = () => {
+  // ignore
+}
+
+const defaultRequiresSocket = () => {
+  return false
+}
+
+const defaultResolve = Callback.resolve
+
+export const handleJsonRpcMessage = async (...args: any[]) => {
+  let message
+  let ipc
+  let execute
+  let preparePrettyError
+  let logError
+  let resolve
+  let requiresSocket
+  if (args.length === 1) {
+    const arg = args[0]
+    message = arg.message
+    ipc = arg.ipc
+    execute = arg.execute
+    preparePrettyError = arg.preparePrettyError || defaultPreparePrettyError
+    logError = arg.logError || defaultLogError
+    requiresSocket = arg.requiresSocket || defaultRequiresSocket
+    resolve = arg.resolve || defaultResolve
+  } else {
+    ipc = args[0]
+    message = args[1]
+    execute = args[2]
+    resolve = args[3]
+    preparePrettyError = args[4]
+    logError = args[5]
+    requiresSocket = args[6]
+  }
   if ('id' in message) {
     if ('method' in message) {
       const response = await GetResponse.getResponse(
