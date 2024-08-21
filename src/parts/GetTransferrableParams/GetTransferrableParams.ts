279650eb29a61f1674abd63837537195a1c8e973
Le Vivilet <levivlet@pm.me>
2024-08-21 22:24:50 +0200

    try to add fn

--- src/parts/GetTransferrableParams/GetTransferrableParams.ts
+++ src/parts/GetTransferrableParams/GetTransferrableParams.ts
@@ -1,1 +1,11 @@
-export const getTransferrableParams = (params: readonly unknown[]) => {}
+import * as IsTransferrable from '../IsTransferrable/IsTransferrable.ts'
+
+export const getTransferrableParams = <T>(params: T): T | undefined => {
+  if (IsTransferrable.isTransferrable(params)) {
+    return params
+  }
+  if (Array.isArray(params)) {
+    return params.filter(IsTransferrable.isTransferrable)
+  }
+  return undefined
+}
