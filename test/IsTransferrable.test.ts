e82ddd29cd74f34d84c27e245be84b67e683350f
Le Vivilet <levivlet@pm.me>
2024-08-21 22:16:48 +0200

    feature: simplify isTransferrable test

--- test/IsTransferrable.test.ts
+++ test/IsTransferrable.test.ts
@@ -1,55 +1,27 @@
-import { expect, test, beforeAll } from '@jest/globals'
-import * as IsTransferrable from '../src/parts/IsTransferrable/IsTransferrable.ts'
+import { expect, jest, test } from '@jest/globals'
 
-beforeAll(() => {
-  // @ts-ignore
-  globalThis.OffscreenCanvas = class {}
-
-  // @ts-ignore
-  globalThis.MessagePort = class {}
+jest.unstable_mockModule('../src/parts/Transferrables/Transferrables', () => {
+  return {
+    transferrables: [
+      (value: unknown) => {
+        return value?.constructor?.name === 'B'
+      },
+    ],
+  }
 })
 
-test('null', () => {
-  const value = null
+const IsTransferrable = await import(
+  '../src/parts/IsTransferrable/IsTransferrable.ts'
+)
+
+test('not transferrable', () => {
+  class A {}
+  const value = new A()
   expect(IsTransferrable.isTransferrable(value)).toBe(false)
 })
 
-test('undefined', () => {
-  const value = undefined
-  expect(IsTransferrable.isTransferrable(value)).toBe(false)
-})
-
-test('number', () => {
-  const value = 12
-  expect(IsTransferrable.isTransferrable(value)).toBe(false)
-})
-
-test('string', () => {
-  const value = 'a'
-  expect(IsTransferrable.isTransferrable(value)).toBe(false)
-})
-
-test('symbol', () => {
-  const value = Symbol()
-  expect(IsTransferrable.isTransferrable(value)).toBe(false)
-})
-
-test('offscreenCanvas', () => {
-  const value = new OffscreenCanvas(0, 0)
+test('transferrable', () => {
+  class B {}
+  const value = new B()
   expect(IsTransferrable.isTransferrable(value)).toBe(true)
-})
-
-test('messagePort', () => {
-  const value = new MessagePort()
-  expect(IsTransferrable.isTransferrable(value)).toBe(true)
-})
-
-test('object', () => {
-  const value = {}
-  expect(IsTransferrable.isTransferrable(value)).toBe(false)
-})
-
-test('array', () => {
-  const value = [] as const
-  expect(IsTransferrable.isTransferrable(value)).toBe(false)
 })
