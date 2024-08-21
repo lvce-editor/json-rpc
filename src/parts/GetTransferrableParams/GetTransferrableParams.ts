cae624f6159f469556e91b4e5e1861b38f62886a
Le Vivilet <levivlet@pm.me>
2024-08-21 22:27:11 +0200

    rename variable

--- src/parts/GetTransferrableParams/GetTransferrableParams.ts
+++ src/parts/GetTransferrableParams/GetTransferrableParams.ts
@@ -1,13 +1,11 @@
 import * as IsTransferrable from '../IsTransferrable/IsTransferrable.ts'
 
-export const getTransferrableParams = (
-  params: unknown,
-): unknown | undefined => {
-  if (IsTransferrable.isTransferrable(params)) {
-    return params
+export const getTransferrableParams = (value: unknown): unknown | undefined => {
+  if (IsTransferrable.isTransferrable(value)) {
+    return value
   }
-  if (Array.isArray(params)) {
-    return params.filter(IsTransferrable.isTransferrable)
+  if (Array.isArray(value)) {
+    return value.filter(IsTransferrable.isTransferrable)
   }
   return undefined
 }
