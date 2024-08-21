8bcd55d640e5f0ca56c02aa6e768905abf20ba67
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-21 22:18:41 +0200

    feature: mark socket as transferrable (#52)

--- src/parts/Transferrables/Transferrables.ts
+++ src/parts/Transferrables/Transferrables.ts
@@ -1,9 +1,11 @@
-import * as IsOffscreenCanvas from '../IsOffscreenCanvas/IsOffscreenCanvas.ts'
-import * as IsMessagePortMain from '../IsMessagePortMain/IsMessagePortMain.ts'
 import * as IsMessagePort from '../IsMessagePort/IsMessagePort.ts'
+import * as IsMessagePortMain from '../IsMessagePortMain/IsMessagePortMain.ts'
+import * as IsOffscreenCanvas from '../IsOffscreenCanvas/IsOffscreenCanvas.ts'
+import * as IsSocket from '../IsSocket/IsSocket.ts'
 
 export const transferrables = [
-  IsOffscreenCanvas.isOffscreenCanvas,
-  IsMessagePortMain.isMessagePortMain,
   IsMessagePort.isMessagePort,
+  IsMessagePortMain.isMessagePortMain,
+  IsOffscreenCanvas.isOffscreenCanvas,
+  IsSocket.isSocket,
 ]
