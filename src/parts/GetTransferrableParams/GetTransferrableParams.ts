62834565060de8dddbc5160b857422235723380f
Le Vivilet <levivlet@pm.me>
2024-08-21 22:25:23 +0200

    use unknown

--- src/parts/GetTransferrableParams/GetTransferrableParams.ts
+++ src/parts/GetTransferrableParams/GetTransferrableParams.ts
@@ -1,6 +1,8 @@
 import * as IsTransferrable from '../IsTransferrable/IsTransferrable.ts'
 
-export const getTransferrableParams = <T>(params: T): T | undefined => {
+export const getTransferrableParams = (
+  params: unknown,
+): unknown | undefined => {
   if (IsTransferrable.isTransferrable(params)) {
     return params
   }
