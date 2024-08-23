5930727da2360c0ba724ac991a51ad1f01f32659
Le Vivilet <levivlet@pm.me>
2024-08-23 21:02:21 +0200

    feature: remove unused logger code

--- src/parts/Logger/Logger.ts
+++ src/parts/Logger/Logger.ts
@@ -1,11 +1,3 @@
-export const info = (...args: any[]) => {
-  console.info(...args)
-}
-
 export const warn = (...args: any[]) => {
   console.warn(...args)
-}
-
-export const error = (...args: any[]) => {
-  console.error(...args)
 }
