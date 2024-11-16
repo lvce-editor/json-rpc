ccbe87052f63a96175601cd235d0851168273ddb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-16 15:39:53 +0100

    feature: improve callback types (#98)

--- src/parts/Callback/Callback.ts
+++ src/parts/Callback/Callback.ts
@@ -3,26 +3,26 @@ import * as CallbackState from '../CallbackState/CallbackState.ts'
 import * as Id from '../Id/Id.ts'
 import * as Logger from '../Logger/Logger.ts'
 
-interface RegisteredPromise {
+interface RegisteredPromise<T = unknown> {
   readonly id: number
-  readonly promise: Promise<unknown>
+  readonly promise: Promise<T>
 }
 
-export const registerPromise = (): RegisteredPromise => {
+export const registerPromise = <T = unknown>(): RegisteredPromise<T> => {
   const id = Id.create()
-  const { resolve, promise } = Promise.withResolvers()
+  const { resolve, promise } = Promise.withResolvers<T>()
   CallbackState.set(id, resolve)
   return { id, promise }
 }
 
-export const resolve = (id: number, args: any): void => {
+export const resolve = (id: number, response: any): void => {
   Assert.number(id)
   const fn = CallbackState.get(id)
   if (!fn) {
-    console.log(args)
+    console.log(response)
     Logger.warn(`callback ${id} may already be disposed`)
     return
   }
-  fn(args)
+  fn(response)
   CallbackState.remove(id)
 }
