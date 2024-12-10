bf4a537550e1fff20481d9ded4ab57be044ddaa2
Le Vivilet <levivlet@pm.me>
2024-12-10 20:49:03 +0100

    test

--- test/CallbackState.test.ts
+++ test/CallbackState.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@jest/globals'
 import * as CallbackState from '../src/parts/CallbackState/CallbackState.ts'
 
-test('clear', async () => {
+test('clear', () => {
   const id = 1
   const fn = () => {}
   CallbackState.set(id, fn)
