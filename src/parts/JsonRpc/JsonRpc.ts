d5a117113598bc536cd917f9dd5418f78ae9777f
Le Vivilet <levivilet@pm.me>
2026-01-10 23:21:08 +0100

    feature: export restoreJsonRpcError

--- src/parts/JsonRpc/JsonRpc.ts
+++ src/parts/JsonRpc/JsonRpc.ts
@@ -2,6 +2,7 @@ import * as JsonRpcEvent from '../JsonRpcEvent/JsonRpcEvent.ts'
 import * as JsonRpcRequest from '../JsonRpcRequest/JsonRpcRequest.ts'
 import * as UnwrapJsonRpcResult from '../UnwrapJsonRpcResult/UnwrapJsonRpcResult.ts'
 import type { IpcConnection } from '../JsonRpcTypes/JsonRpcTypes.ts'
+import { restoreJsonRpcError } from '../RestoreJsonRpcError/RestoreJsonRpcError.ts'
 
 const invokeHelper = async <T>(
   ipc: IpcConnection,
@@ -49,4 +50,5 @@ export { registerPromise } from '../RegisterPromise/RegisterPromise.ts'
 export { getErrorResponse } from '../GetErrorResponse/GetErrorResponse.ts'
 export { getSuccessResponse } from '../GetSuccessResponse/GetSuccessResponse.ts'
 export { handleJsonRpcMessage } from '../HandleJsonRpcMessage/HandleJsonRpcMessage.ts'
+export {restoreJsonRpcError} from '../RestoreJsonRpcError/RestoreJsonRpcError.ts'
 export { JsonRpcEvent, JsonRpcRequest }
