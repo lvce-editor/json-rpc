4b430f80607405d59a910f32f3f05cc2a4f69116
Le Vivilet <levivilet@pm.me>
2026-08-02 01:00:50 +0000

    feature: update eslint dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
 
-export default [
+export default defineConfig([
   ...config.default,
   {
     rules: {
@@ -24,4 +25,4 @@ export default [
   {
     ignores: ['src/index.d.ts'],
   },
-]
+])
